a11ffa0beff8e030dbfd0e58ae4e66c0d068d670
Manuel <mail@lmaos.de>
2020-01-18 11:30:14 +0100

    Made Project GitHub compatible, by removing all hardcoded Filepaths and deleting Controller Lib as ist is not needed

--- src/de/lmaos/_code/lora/Lora.java
+++ src/de/lmaos/_code/lora/Lora.java
@@ -1,7 +1,6 @@
 package de.lmaos._code.lora;
 
-import com.studiohartman.jamepad.ControllerManager;
-import com.studiohartman.jamepad.ControllerState;
+
 import name.panitz.game.framework.*;
 
 import java.awt.*;
@@ -22,7 +21,7 @@ public class Lora<I,S> extends AbstractGame<I, S> {
 		File resFolder = new File("src/res");
 		List<ImageObject<I>> tmp = new ArrayList<>();
 		getGOss().add(tmp);
-		MapObject<I> map = new MapObject<>("C:\\Users\\Manuel\\Google Drive\\Subato Game (Name Pending)\\Lora\\src\\res\\maps\\map1\\map1.png"
+		MapObject<I> map = new MapObject<>("src\\res\\maps\\map1\\map1.png"
 			, new Vertex(0,0),new Vertex(0,0));
 
 		List<MapObject<I>> mapObjects = new ArrayList<>();
@@ -33,13 +32,9 @@ public class Lora<I,S> extends AbstractGame<I, S> {
 
 	@Override
 	public void doChecks() {
-/*		ControllerState state = controllerManager.getState(0);
-		emulateKeyPress(state);*/
+
 	}
 
-	private void emulateKeyPress(ControllerState state) {
-		System.out.println(state);
-	}
 
 	@Override
 	public void paintTo(GraphicsTool<I> g) {
